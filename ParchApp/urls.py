d69f885dc2cea99010a20917ffcef294b61fad90
sbetancurb <sbetancurb@eafit.edu.co>
2023-02-14 10:48:53 -0500

    cambios

--- ParchApp/urls.py
+++ ParchApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ParchApp2 import views as ParchaViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ParchaViews.main),
 ]
