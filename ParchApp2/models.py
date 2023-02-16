c2404ad45acf052f1154f1ed274eb7df5152f5d2
sbetancurb <sbetancurb@eafit.edu.co>
2023-02-16 09:57:06 -0500

    cambios

--- ParchApp2/models.py
+++ ParchApp2/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class lugares(models.Model):
+    nombre = models.CharField(max_length = 50, blank = False, null = False)
+    foto = models.ImageField()
+    LvlEconomico = models.CharField(max_length = 50, blank = False, null = False)
+    descripcion = models.CharField(max_length = 140, blank = False, null = False)
+    ubicacion = models.CharField(max_length = 50, blank = False, null = False)
+
